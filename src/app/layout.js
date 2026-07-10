871212b4d513190ddbed5ddfbadde8622e742e4f
JoseRodriguesF <jose.antonio.220507@gmail.com>
2026-07-10 15:37:11 -0300

    chore(observabilidade): adicionar vercel speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SmoothScroll from "@/components/SmoothScroll";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
           {children}
         </main>
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
